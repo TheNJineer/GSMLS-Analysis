c8feb45d4773309c5d166f7d12d35d623f71b12e
Jibreel Hameed <jqhameed@gmail.com>
2026-01-23 18:19:08 +0000

    - Major and minor jobs used for DockerOperators

---
+++ jobs/major_jobs/gsmls_image_consumer.py
@@ -0,0 +1,18 @@
+import sys
+from gsmls_core.gsmls.Kafka_GSMLSConsumer import KafkaGSMLSConsumer
+from gsmls_core.gsmls.utility_func import check_pipeline_metadata
+
+
+if __name__ == "__main__":
+
+    # Add logger back into the class script
+    img_consumer = KafkaGSMLSConsumer()
+    results = img_consumer.main(prop="IMAGES", logger=None, retry=False)
+
+    if not isinstance(results, bool):
+        # Need to be able to log something here
+        sys.exit(1)
+    else:
+        check_pipeline_metadata("gsmls_airflow_pipeline", key="image_consumer", status=results)
+        sys.exit(0)
+
