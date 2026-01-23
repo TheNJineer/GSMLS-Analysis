24e20f13f9ec991ecab988de4c8b0f2333c0fa66
Jibreel Hameed <jqhameed@gmail.com>
2026-01-23 18:25:33 +0000

    - Minor update. Go to v0.6.0 (remove the gsmls_core in the imports)

--- gsmls_core/gsmls/GSMLS.py
+++ gsmls_core/gsmls/GSMLS.py
@@ -1,4 +1,3 @@
-import json
 import re
 import selenium.common.exceptions
 import time
@@ -11,7 +10,7 @@ import pandas as pd
 import sys, traceback
 import logging
 from dotenv import load_dotenv
-from gsmls.utility_func import (
+from gsmls_core.gsmls.utility_func import (
     create_sql_engine,
     create_kafka_producer, logger_decorator,
     create_selenium_webdriver, get_filepath
@@ -23,7 +22,7 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import date
 from pendulum import timezone
-from gsmls.utility_func import get_us_pw
+from gsmls_core.gsmls.utility_func import get_us_pw
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
