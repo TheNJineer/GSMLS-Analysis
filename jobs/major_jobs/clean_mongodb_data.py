69cb44808e669eb8ed57710f8b7a2fb08812f8a6
Jibreel Hameed <jqhameed@gmail.com>
2026-02-10 22:13:45 +0000

    - Minor update to v0.7.0

--- jobs/major_jobs/clean_mongodb_data.py
+++ jobs/major_jobs/clean_mongodb_data.py
@@ -1,6 +1,6 @@
 import argparse
-from gsmls_core.gsmls.RealEstateImages import RealEstateImages
-from gsmls_core.gsmls.utility_func import check_pipeline_metadata, cutoff_time
+from gsmls.RealEstateImages import RealEstateImages
+from gsmls.utility_func import cutoff_time
 
 
 def parse_args():
@@ -14,15 +14,15 @@ def parse_args():
 if __name__ == "__main__":
 
     args = parse_args()
-    program_cutoff = cutoff_time(minutes=41, tz="America/New_York")
+    program_cutoff = cutoff_time(hours=4, minutes=35, tz="America/New_York")
 
     if bool(args.local) is False:
         print(' ==== CLEANING THE MONGODB ATLAS DATABASE ==== ')
-        RealEstateImages().database_cleanup(cutoff_time=program_cutoff)
+        RealEstateImages(latest_order_num=64872924).database_cleanup(cutoff_time=program_cutoff)
     else:
         # Initiates two separate objects both querying data from a MongoDB Atlas
         # and a Docker container local connection respectively
         print(' ==== CLEANING THE MONGODB ATLAS & DOCKER DATABASE ==== ')
-        RealEstateImages().database_cleanup(cutoff_time=program_cutoff)
-        RealEstateImages(local=True).database_cleanup(cutoff_time=program_cutoff)
+        RealEstateImages(latest_order_num=64872924).database_cleanup(cutoff_time=program_cutoff)
+        RealEstateImages(latest_order_num=64872924, local=True).database_cleanup(cutoff_time=program_cutoff)
 
