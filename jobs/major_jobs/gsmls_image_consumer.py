67560be884a0f89d6f567f6cb24e525ad966792f
Jibreel Hameed <jqhameed@gmail.com>
2026-02-18 15:47:58 +0000

    - Update to include processing of a checkpoint file and handle processing errors

--- jobs/major_jobs/gsmls_image_consumer.py
+++ jobs/major_jobs/gsmls_image_consumer.py
@@ -1,18 +1,30 @@
 import sys
+import os
 from gsmls.Kafka_GSMLSConsumer import KafkaGSMLSConsumer
-from gsmls.utility_func import check_pipeline_metadata
+from gsmls.utility_func import get_filepath, check_pipeline_metadata
 
 
 if __name__ == "__main__":
 
+    base_path = get_filepath('backups')
+    filepath = os.path.join(base_path, f'prop_images.xlsx')
+    retry = False
+    print(f' ==== CURRENT RETRY STATUS: {retry} ==== ')
+
+    if os.path.isfile(filepath):
+        retry = True
+        print(f' ==== BACKUP DATA EXISTS: {retry} ==== ')
+
     # Add logger back into the class script
     img_consumer = KafkaGSMLSConsumer()
-    results = img_consumer.main(prop="IMAGES", logger=None, retry=False)
 
-    if not isinstance(results, bool):
-        # Need to be able to log something here
-        sys.exit(1)
-    else:
-        check_pipeline_metadata("gsmls_airflow_pipeline", key="image_consumer", status=results)
-        sys.exit(0)
+    while True:
+        results = img_consumer.main(prop="IMAGES", logger=None, retry=retry)
+
+        if not isinstance(results, bool):
+            # Need to be able to log something here
+            sys.exit(1)
+        else:
+            check_pipeline_metadata("gsmls_airflow_pipeline", key="image_consumer", status=results)
+            sys.exit(0)
 
