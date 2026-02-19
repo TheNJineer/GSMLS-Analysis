bb282a56afaee81cdff975c7510039f23922b250
Jibreel Hameed <jqhameed@gmail.com>
2026-02-19 21:40:14 +0000

    - Minor update v0.9.0 - Update check_pipeline_metadata

--- jobs/major_jobs/gsmls_image_consumer.py
+++ jobs/major_jobs/gsmls_image_consumer.py
@@ -1,11 +1,22 @@
+import argparse
 import sys
 import os
 from gsmls.Kafka_GSMLSConsumer import KafkaGSMLSConsumer
 from gsmls.utility_func import get_filepath, check_pipeline_metadata
 
 
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='Consume image data from Apache Kafka into MongoDB')
+    parser.add_argument("--prop_type", required=True)
+
+    # return parser.parse_args(['--prop_type', 'RES'])
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
 
+    args = parse_args()
     base_path = get_filepath('backups')
     filepath = os.path.join(base_path, f'prop_images.xlsx')
     retry = False
@@ -25,6 +36,7 @@ if __name__ == "__main__":
             # Need to be able to log something here
             sys.exit(1)
         else:
-            check_pipeline_metadata("gsmls_airflow_pipeline", key="image_consumer", status=results)
+            check_pipeline_metadata("gsmls_airflow_pipeline", prop_type=args.prop_type,
+                                    key="image_consumer", status=results)
             sys.exit(0)
 
